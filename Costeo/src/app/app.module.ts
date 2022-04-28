605ea039b7b28ae777dd3c6574f460191eef9aa6
gusgoludo305@hotmail.com <gusgoludo305@hotmail.com>
2022-04-28 16:56:49 -0500

    Se creo la carpeta Costeo donde se creo el componente header

--- Costeo/src/app/app.module.ts
+++ Costeo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CosteoHeaderComponent } from './costeo/header/costeo-header/costeo-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CosteoHeaderComponent
   ],
   imports: [
     BrowserModule,

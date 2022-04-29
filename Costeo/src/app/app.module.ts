056fd197739c84513ee119e5ef8d89d1c0340a13
Gustavo-Vazquez9 <75765329+Gustavo-Vazquez9@users.noreply.github.com>
2022-04-29 11:30:03 -0500

    Update app.module.ts

--- Costeo/src/app/app.module.ts
+++ Costeo/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CosteoHeaderComponent } from './costeo/header/costeo-header/costeo-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CosteoHeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

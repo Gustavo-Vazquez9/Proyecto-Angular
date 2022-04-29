a728326f365fd5a19e9afc7be794c5ae3c639a3a
Ari Abril <occamppo.3025@gmail.com>
2022-04-29 17:15:48 -0500

    input pero faltan detallitos

--- Costeo/src/app/app.module.ts
+++ Costeo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './Input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,

3d1d81c26d7b206b60ab98d7d65db3b471a496ff
Daniela Lara <laradni@icloud.com>
2022-05-03 17:03:52 -0500

    Se agrego una interface

--- Costeo/src/app/components/login/login.component.ts
+++ Costeo/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output} from '@angular/core';
 import { ServiceService } from 'src/app/Services/service.service';
 import { HttpClient } from '@angular/common/http';
+import { Users } from '../Interfaces/user.interface';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +14,20 @@ export class LoginComponent{
   password: string = '';
   passwordConfirm: string = '';
   email: string = '';
+  // users: Users[];
 
-
-  constructor(private service:ServiceService) { }
+  constructor(private service:ServiceService) { 
+    this.service.obtenerUsusarios().subscribe((respuesta)=>{
+        console.log(respuesta);
+    });
+  }
 
   ingresarUsuario(){
     console.log(this.user);
+    const newUser: Users = {"usuario": this.user, "correo": this.email, "contraseña": this.password }
+    this.service.insertarUsusario(newUser).subscribe((respuesta)=>{
+      console.log(respuesta)
+    });
   }
 
 }

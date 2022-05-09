8dae9fa2e799d12a2d8e81686bef2b7c55f52dca
Emma Rivera <earu@gft.com>
2022-05-09 17:29:34 -0500

    Actualizacion

--- Costeo/src/app/app.module.ts
+++ Costeo/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormIngredientesModule } from './form-ingredientes/form-ingredientes.module';
 import { TablaPreciosModule } from './tabla-precios/tabla-precios.module';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { TablaPreciosModule } from './tabla-precios/tabla-precios.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormIngredientesModule,
     TablaPreciosModule
   ],
   providers: [],

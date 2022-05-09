1b9b055a8ee27af102724c0c72b8473f57b9c8be
Emma Rivera <earu@gft.com>
2022-05-09 15:39:48 -0500

    actualizaciones

--- Costeo/src/app/app.module.ts
+++ Costeo/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormIngredientesModule } from './form-ingredientes/form-ingredientes.module';
 import { TablaPreciosModule } from './tabla-precios/tabla-precios.module';
-
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { TablaPreciosModule } from './tabla-precios/tabla-precios.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormIngredientesModule,
     TablaPreciosModule
   ],
   providers: [],
--- Costeo/src/app/tabla-precios/tabla/tabla.component.ts
+++ Costeo/src/app/tabla-precios/tabla/tabla.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tabla',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
 }
+

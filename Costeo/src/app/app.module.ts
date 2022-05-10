3af96b62f8326a1b760161579ca562ed692e78f7
gusgoludo305@hotmail.com <gusgoludo305@hotmail.com>
2022-05-10 10:53:45 -0500

    .

--- Costeo/src/app/app.module.ts
+++ Costeo/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaPreciosComponent } from './tabla-precios/tabla-precios.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TablaPreciosComponent,
 
   ],
   exports: [

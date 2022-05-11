462f9fde69c3e43e4e2d630abf1660bbd2c012e8
JosMan9 <josem_rl@hotmail.com>
2022-05-11 14:24:57 -0500

    Commit bugs

--- Costeo/src/app/form-ingredientes/form-ingredientes.component.ts
+++ Costeo/src/app/form-ingredientes/form-ingredientes.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { ApiService } from '../../service/api.service';
 import { Ingrediente } from '../../interface/Ingrediente';
 
@@ -10,12 +17,21 @@ import { Ingrediente } from '../../interface/Ingrediente';
 export class FormIngredientesComponent {
   ingredientes: Ingrediente[] = [];
   unidades: String[] = [];
+  ingrediente_: Ingrediente = {
+    id: 0,
+    name: '',
+    img: '',
+    unity: [],
+    price: 0.0,
+    weight: 0.0,
+    amount: 0,
+  };
 
   ingrediente: String;
-  imagen: String = "../../assets/img/fruta.png";
-  seleccionUnidad = "";
-  disabledButton: boolean;
-  cantidadInput = "";
+  imagen: String = '../../assets/img/fruta.png';
+  seleccionUnidad = '';
+  disabledButton: boolean = true;
+  cantidadInput = '';
 
   @ViewChild('ingredienteInput') inputIngrediente: ElementRef;
   @ViewChild('cantidadInput') inputCantidad: ElementRef;
@@ -23,13 +39,12 @@ export class FormIngredientesComponent {
   @ViewChild('pesoInput') inputPeso: ElementRef;
   @ViewChild('selecUnidad') selecUnidad: ElementRef;
 
-
-  @Output('ingredienteNombre') ingredienteNombre: String;
-  @Output('cantidadPersonas') cantidad: number;
-  @Output('unidad') unidad: String;
-  @Output('precioNumero') precio: number;
-  @Output('pesoNumero') peso: number;
-
+  @Output() onIngredienteNombre: EventEmitter<string> = new EventEmitter();
+  @Output() onCantidadNombre: EventEmitter<number> = new EventEmitter();
+  @Output() onUnidadNombre: EventEmitter<string> = new EventEmitter();
+  @Output() onPrecioNombre: EventEmitter<number> = new EventEmitter();
+  @Output() onTotal: EventEmitter<number> = new EventEmitter();
+  // @Output() ingrediente_output = new EventEmitter<Ingrediente>();
 
   constructor(private service: ApiService) {
     this.service.getIngredientes().subscribe((respuesta) => {
@@ -38,13 +53,10 @@ export class FormIngredientesComponent {
   }
 
   getIngredienteInput() {
-    this.ingrediente = this.inputIngrediente.nativeElement.value
-    console.log(this.ingrediente);
-    console.log(this.ingredientes);
+    this.ingrediente = this.inputIngrediente.nativeElement.value;
     let ingredienteMayuscula = this.ingrediente.toUpperCase();
 
-    for(let i = 0; i < this.ingredientes.length; i++) {
-
+    for (let i = 0; i < this.ingredientes.length; i++) {
       if (ingredienteMayuscula === this.ingredientes[i].name.toUpperCase()) {
         this.inputPeso.nativeElement.value = this.ingredientes[i].weight;
         this.inputPrecio.nativeElement.value = this.ingredientes[i].price;
@@ -52,26 +64,39 @@ export class FormIngredientesComponent {
         this.imagen = this.ingredientes[i].img;
 
         this.cantidadInput = this.inputCantidad.nativeElement.value;
-        console.log(this.cantidadInput);
 
-        if( this.cantidadInput === "") {
-          this.disabledButton = true;
-        } else {
-          this.disabledButton = false;
-        }
-        
+        this.ingrediente_.id = this.ingredientes[i].id;
+        this.ingrediente_.img = this.ingredientes[i].img;
+        this.ingrediente_.name = this.ingredientes[i].name;
+        this.ingrediente_.price = this.ingredientes[i].price;
+        this.ingrediente_.weight = this.ingredientes[i].weight;
+
+
+        break;
       } else {
-        this.disabledButton = true
+        this.disabledButton = true;
       }
     }
-
   }
 
   getUnidad() {
-  this.seleccionUnidad = this.selecUnidad.nativeElement.value;
+    this.seleccionUnidad = this.selecUnidad.nativeElement.value;
+    this.ingrediente_.unity = this.selecUnidad.nativeElement.value;
+
+    if (this.seleccionUnidad === 'primera_opcion') {
+      console.log("entree");
+      this.disabledButton = true;
+    } else {
+      this.disabledButton = false;
+
+    }
   }
 
   clickBoton() {
-    console.log("holaaa");
+    this.onIngredienteNombre.emit(this.ingrediente_.name);
+    this.onCantidadNombre.emit(this.ingrediente_.amount);
+    this.onUnidadNombre.emit(this.seleccionUnidad);
+    this.onPrecioNombre.emit(this.ingrediente_.price);
+    this.onTotal.emit(this.ingrediente_.weight);
   }
 }
--- Costeo/src/app/tabla-precios/tabla-precios.component.ts
+++ Costeo/src/app/tabla-precios/tabla-precios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingrediente } from '../../interface/Ingrediente';
+import { Ingrediente} from '../../interface/Ingrediente';
+// import { ingre } from '../../interface/Ingrediente';
 
 @Component({
   selector: 'app-tabla-precios',
@@ -7,11 +8,46 @@ import { Ingrediente } from '../../interface/Ingrediente';
   styleUrls: ['./tabla-precios.component.scss']
 })
 export class TablaPreciosComponent implements OnInit {
-  lista_ingredientes: Ingrediente[] = []
+  lista_ingredientes: any[] = []
+  //[['Jitomate',3],['Limon',4]]
+  ingrediente : string = "";
+  cantidad : number = 0;
+  unidad : string = "";
+  precio : number = 0;
+  total : number = 0;
 
-  constructor() { }
+
+
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  recibirNombre(recibeIngrediente:string){
+     this.ingrediente=recibeIngrediente;
+    // console.log(this.ingrediente);
+  }
+
+  recibirCantidad(recibeCantidad:number){
+    this.cantidad=recibeCantidad;
+
+    // console.log(recibeCantidad)
+    // console.log(this.ingrediente);
+  }
+
+  recibirUnidad(recibeUnidad:string){
+    this.unidad=recibeUnidad;
+
+  }
+
+  recibirPrecio(recibePrecio:number){
+    this.precio=recibePrecio;
+
+  }
+
+  esTotal(recibeTotal:number){
+    this.total=recibeTotal;
+    this.lista_ingredientes.push([this.ingrediente,this.cantidad,this.unidad,this.precio,this.total]);
   }
 
 }

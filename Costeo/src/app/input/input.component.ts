d66058cbd5060b0293c113e57fe5be8ee5dfae97
Ari Abril <occamppo.3025@gmail.com>
2022-05-12 15:40:06 -0500

    Componente Input actualizado

--- Costeo/src/app/input/input.component.ts
+++ Costeo/src/app/input/input.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent implements OnInit {
+export class InputComponent {
+
+  @Output() onPlatillo : EventEmitter<string> = new EventEmitter();
+  @Output() onPersonas : EventEmitter<number> = new EventEmitter();
+  activarPersona : number = 0;
+
+  nombrePlat : string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  personas( numero : number ){
+    this.activarPersona = numero;
+  }
+
+  activar( num : number ) : string {
+    return( num === this.activarPersona)
+      ? 'activo'
+    : 'normal'
+  }
+
+  ingresarPlat(){
+    this.onPlatillo.emit(this.nombrePlat);
+    this.onPersonas.emit(this.activarPersona);
   }
 
 }

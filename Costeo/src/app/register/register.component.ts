e5e506676580b965c0bf957423191fd83d3424d8
Gustavo <gusgoludo9@gmail.com>
2022-05-13 12:07:16 -0500

    se agrego el cargando a registrarse

--- Costeo/src/app/register/register.component.ts
+++ Costeo/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output} from '@angular/core';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 import { Users } from '../Interfaces/user.interface';
+import {interval, timer} from 'rxjs'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -13,8 +15,10 @@ export class RegisterComponent{
   passwordConfirm: string = '';
   email: string = '';
   error: boolean = false;
+  registrado : boolean = false;
+  tiempo : any = timer(5000);
 
-  constructor(private servicio : UsuariosService) { }
+  constructor(private servicio : UsuariosService, private _router: Router) { }
 
   ingresarUsuario(){
     const newUser: Users =
@@ -25,9 +29,17 @@ export class RegisterComponent{
     }
     if(this.password === this.passwordConfirm)
     {
-      this.servicio.insertarUsusario(newUser).subscribe((respuesta)=>{
+      /* this.servicio.insertarUsusario(newUser).subscribe((respuesta)=>{
         console.log(respuesta)
+      }); */
+      this.tiempo.subscribe( () =>
+      {
+        console.log("false");
+        this.registrado=false;
+        this._router.navigate(['/login']);
       });
+      console.log("true");
+      this.registrado=true;
       this.error = false;
     }else{
       this.error= true;
